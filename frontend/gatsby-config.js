95bb191fe6e2696ff4abf48b604db8bc8b0f101d
sahbic <sahbingo@gmail.com>
2020-05-22 18:25:54 +0200

    set up strapi for gatsby

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -26,7 +26,7 @@ module.exports = {
         contentTypes: [
           // List of the Content Types you want to be able to request from Gatsby.
           "recipe",
-          "user",
+          "ingredient",
         ],
         queryLimit: 1000,
       },
